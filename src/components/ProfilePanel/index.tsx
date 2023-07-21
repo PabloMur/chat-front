181c3462e9a7b163f70b65a29ee8f91e2ad9c8bc
PabloMur <polillomurillo@gmail.com>
2023-07-21 13:17:28 -0300

    de a poco va queriendo

--- src/components/ProfilePanel/index.tsx
+++ src/components/ProfilePanel/index.tsx
@@ -2,13 +2,24 @@ import { UserEmail } from "../../ui/UserEmail";
 import { UserImage } from "../../ui/UserImage";
 import { UserName } from "../../ui/UserName";
 import css from "./styles.module.css";
+import { useGoTo } from "../../hooks";
+
 export const ProfilePanel = () => {
+  const goTo = useGoTo();
+  const handleClick = () => {
+    goTo("/settings");
+  };
   return (
     <>
       <div className={css.root}>
         <UserImage />
-        <UserName />
-        <UserEmail />
+        <div>
+          <UserName />
+          <UserEmail />
+          <button className={css.editButton} onClick={handleClick}>
+            Editar Datos
+          </button>
+        </div>
       </div>
     </>
   );
