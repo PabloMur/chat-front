d894921f2a9b78164056de2efcd7975baa74138b
PabloMur <polillomurillo@gmail.com>
2023-07-26 20:06:47 -0300

    loader en password

--- src/pages/Password/index.tsx
+++ src/pages/Password/index.tsx
@@ -1,9 +1,11 @@
 import css from "./styles.module.css";
 import { LoginForm } from "../../components/LoginForm";
+import { Loader } from "../../components/loader";
 export const PasswordPage = () => {
   return (
     <>
       <div className={css.root}>
+        <Loader />
         <LoginForm />
       </div>
     </>
