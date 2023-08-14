1f2a1fd17433801aece8e920fa534ee93d78aeb8
PabloMur <polillomurillo@gmail.com>
2023-08-14 14:25:16 -0300

    user image al obtener la data

--- src/components/ProfilePanel/index.tsx
+++ src/components/ProfilePanel/index.tsx
@@ -3,19 +3,35 @@ import { UserImage } from "../../ui/UserImage";
 import { UserName } from "../../ui/UserName";
 import css from "./styles.module.css";
 import { useGetUserMe, useGoTo } from "../../hooks";
-import { useRecoilValue } from "recoil";
-import { emailAtom, userTokenAtom } from "../../atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  emailAtom,
+  userImageAtom,
+  userNameAtom,
+  userTokenAtom,
+} from "../../atoms";
 import { useEffect } from "react";
+import { Loader } from "../loader";
+import { loaderAtom } from "../../atoms/uiAtoms";
 
 export const ProfilePanel = () => {
   const goTo = useGoTo();
   const token = useRecoilValue(userTokenAtom);
   const email = useRecoilValue(emailAtom);
+  const setLoader = useSetRecoilState(loaderAtom);
+  const setUserName = useSetRecoilState(userNameAtom);
+  const setUserProfileImage = useSetRecoilState(userImageAtom);
 
   useEffect(() => {
     const getUserData = async () => {
-      const test = await useGetUserMe(email, token);
-      if (test) console.log(test);
+      setLoader(true);
+      const data = await useGetUserMe(email, token);
+      const userData = data.userData;
+      if (data) {
+        setUserName(userData.name);
+        setUserProfileImage(userData.userImage);
+      }
+      setLoader(false);
     };
     getUserData();
   }, []);
@@ -25,6 +41,7 @@ export const ProfilePanel = () => {
   };
   return (
     <>
+      <Loader></Loader>
       <div className={css.root}>
         <UserImage />
         <div>
