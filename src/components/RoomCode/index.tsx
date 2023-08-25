d580f67f7d32602ff32d2e346e50f9f951c9e1b1
PabloMur <polillomurillo@gmail.com>
2023-08-24 23:28:17 -0300

    se normaliza room code y dots de modal

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -2,13 +2,14 @@ import css from "./styles.module.css";
 import { useRecoilValue } from "recoil";
 import { roomIdAtom } from "../../atoms";
 import { DeleteRoomButton } from "../../ui/deleteRoomButton";
+import { CustomTitle } from "../../ui/customTitle";
 
 export const RoomCode = () => {
   const roomCode = useRecoilValue(roomIdAtom);
   return (
     <>
       <div className={css.root}>
-        <h2>ROOM: {roomCode}</h2>
+        <CustomTitle>ROOM: {roomCode}</CustomTitle>
         <DeleteRoomButton></DeleteRoomButton>
       </div>
     </>
